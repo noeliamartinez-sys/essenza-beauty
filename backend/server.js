060d6eb6fdbf67dfb4574b0b8627a0dc0ec9128f
Ada Novas <ada.novasflorian@gmail.com>
2026-06-04 18:29:02 -0300

    Configurar conexión con MySQL y Sequelize

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 const app = require("./app");
+const sequelize = require("./config/database");
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor funcionando en puerto ${PORT}`);
-});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Conexión a MySQL exitosa");
+
+    app.listen(PORT, () => {
+      console.log(`Servidor funcionando en puerto ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error al conectar con MySQL:", error);
+  });
